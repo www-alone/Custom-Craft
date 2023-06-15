9855690ef65e7f0b8826b32b92e9292fb9e0110b
nikita <n.m.dyachenko@mail.ru>
2023-06-15 12:15:05 +0300

    update mailer and more

---
+++ mail.php
@@ -0,0 +1,41 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $mail = new PHPMailer(true);
+  try {
+    // Настройка SMTP
+    $email = $_POST['email'];
+    $message = $_POST['notes'];
+    $mail->isSMTP();
+    $mail->Host = 'smtp.yandex.ru';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'www-pain@yandex.ru';
+    $mail->Password = 'duisahdsuiahduiashduiashdiuas';
+    $mail->SMTPSecure = 'TSL';
+    $mail->Port = 587;
+
+    // Получатель и отправитель
+    $mail->setFrom($_POST['email'], $_POST['name']);
+    $mail->addAddress('www-pain@yandex.ru');
+
+    // Контент письма
+    $mail->CharSet = 'UTF-8';
+    $mail->isHTML(true);
+    $mail->Subject = 'Custom Craft';
+    $mail->Body = "<h1>Email: $email</h1><br><h2> Сообщение: $message</h2>";
+    // Отправка
+    // Отправка письма
+    if ($mail->send()) {
+        echo 'Письмо успешно отправлено.';
+        header('Location: page1.html');
+    } else {
+        echo 'Ошибка отправки письма: ' . $mail->ErrorInfo;
+    }
+    } catch (Exception $e) {
+        echo 'Произошла ошибка: ' . $e->getMessage();
+    }
+  }
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4eb532b1fd944fde5bca70f61463ee037e4d1d64',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4eb532b1fd944fde5bca70f61463ee037e4d1d64',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
