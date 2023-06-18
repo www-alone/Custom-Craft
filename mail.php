60e895535e8f6a6a96def8cd1c7e9cdf7953c826
nikita <n.m.dyachenko@mail.ru>
2023-06-18 14:30:09 +0300

    mailer update

--- mail.php
+++ mail.php
@@ -1,40 +1,41 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  
+  require 'vendor/autoload.php';
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $mail = new PHPMailer(true); // Параметр 'true' разрешает модель исключений
+    try {
+      $email = $_POST['email'];
+      $message = $_POST['notes'];
+      $mail->isSMTP();
+      $mail->Host = 'smtp.yandex.ru';// вместо Yandex указываем свою почту
+      $mail->SMTPAuth = true;
+      $mail->Username = ''; // Логин почты
+      $mail->Password = ''; // Пароль
+      $mail->SMTPSecure = 'TLS'; // Включение шифрования TLS, как вариант можно 'ssl'
+      $mail->Port = 587; // TCP-порт, для Яндекса работает именно такой 587
+  
+      // Получатели
+      $mail->setFrom($_POST['email'], $_POST['name']);// Получатель
+      $mail->addAddress('');// Отправитель(почта компании)
 
-require 'vendor/autoload.php';
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $mail = new PHPMailer(true);
-  try {
-    // Настройка SMTP
-    $email = $_POST['email'];
-    $message = $_POST['notes'];
-    $mail->isSMTP();
-    $mail->Host = 'smtp.yandex.ru';//SMTP-почты
-    $mail->SMTPAuth = true;
-    $mail->Username = '';//Почта
-    $mail->Password = '';//Пароль от почты
-    $mail->SMTPSecure = 'TSL';
-    $mail->Port = 587;
-
-    // Получатель и отправитель
-    $mail->setFrom($_POST['email'], $_POST['name']);// Получатель
-    $mail->addAddress('www-pain@yandex.ru');// Отправитель
-
-    // Контент письма
-    $mail->CharSet = 'UTF-8';
-    $mail->isHTML(true);
-    $mail->Subject = 'Custom Craft';
-    $mail->Body = "<h1>Email: $email</h1><br><h2> Сообщение: $message</h2>";
-    // Отправка письма
-    if ($mail->send()) {
-        echo 'Письмо успешно отправлено.';
-        header('Location: page1.html');
-    } else {
-        echo 'Ошибка отправки письма: ' . $mail->ErrorInfo;
-    }
+      // Контент письма
+      $mail->CharSet = 'UTF-8'; // Кодировка для всех текстов
+      $mail->isHTML(true); // Включение HTML-формата
+      $mail->Subject = 'VrCenter';
+      $mail->Body = "<h1>Email: $email</h1><br><h2> Сообщение: $message</h2>";
+  
+      // Отправка
+      $mail->send();
+      header("page1.html");
+      die();
+      echo 'Письмо отправлено';
+  
     } catch (Exception $e) {
-        echo 'Произошла ошибка: ' . $e->getMessage();
+      echo $mail->ErrorInfo;   
+      header("page1.html");
+      die();
     }
   }
 ?>
